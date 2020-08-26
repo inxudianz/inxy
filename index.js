bba80eb1b8c39479cbcaf15c94c7b29a16025813
Inxudianz <william.inxcorp@gmail.com>
2020-08-26 17:53:55 +0700

    add server info command

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (message.content === config.prefix + 'ping') {
 		message.channel.send('Pong.');
+	} else if (message.content === config.prefix + 'server') {
+		message.channel.send('Hey, I\'m currently on ' + message.guild.name);
 	}
 });
 client.login(config.token);
